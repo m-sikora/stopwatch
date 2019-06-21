62ed0fcdc408d345f832a4e8967e1289798281af
msikora <michal.sikora@arteia.co>
2019-06-21 11:00:29 +0200

    initial

---
+++ index.js
@@ -0,0 +1,80 @@
+const util = require('util');
+const _ = require('lodash');
+const convertHrtime = require('convert-hrtime');
+const math = require('mathjs');
+
+function Scope() {
+  this.buckets = {};
+  this.checkpoints = {};
+  this.registerCheckpoint = function (name) {
+    this.checkpoints[name] = process.hrtime();
+  };
+  this.getPrettyDelta = function (checkpointName) {
+    return convertHrtime(this.getDelta(...arguments));
+  };
+  this.getDelta = function (checkpointName) {
+    const checkpoint = this.checkpoints[checkpointName];
+    if (!checkpoint) {
+      throw new Error('Checkpoint not found');
+    }
+    const delta = process.hrtime(checkpoint);
+    return delta;
+  };
+  this.getBucket = function (bucketName) {
+    return this.buckets[bucketName] || { times: [] };
+  };
+  this.saveDelta = function (checkpointName, bucketName) {
+    const bucket = this.getBucket(bucketName);
+    const delta = this.getDelta(checkpointName);
+    bucket.times.push(delta);
+    this.buckets[bucketName] = bucket;
+  };
+  this.wrapFunction = function (bucketName, callback) {
+    return async () => {
+      this.registerCheckpoint(bucketName);
+      const r = await callback();
+      this.saveDelta(bucketName, bucketName);
+      return r
+    }
+  };
+  this.getPrettyBucket = function (bucketName) {
+    const { times = [] } = this.getBucket(bucketName);
+    const timesMs = times.map(t => convertHrtime(t).milliseconds);
+    const mean = math.mean(timesMs);
+    const std = math.std(timesMs);
+    const max = math.max(timesMs);
+    const min = math.min(timesMs);
+    const count = timesMs.length;
+    return { mean, std, max, min, count };
+  };
+  this.getPrettyBuckets = function () {
+    const buckets = _.chain(this.buckets)
+      .keys()
+      .map(bucketName => {
+        return {
+          bucketName,
+          ...this.getPrettyBucket(bucketName),
+        };
+      })
+      .sortBy(({ mean = 0 }) => -mean)
+      .value();
+    return buckets;
+  };
+  this.prettyPrint = function () {
+    console.log(util.inspect(this.getPrettyBuckets(), { colors: true }));
+  };
+  this.attachPrintHook = function () {
+    process.on('SIGINT', () => {
+      this.prettyPrint();
+    });
+  };
+}
+
+const globalScope = new Scope();
+
+const api = {
+  make: () => new Scope(),
+  getGlobal: () => globalScope,
+};
+
+module.exports = api;
---
+++ test.js
@@ -0,0 +1,45 @@
+const src = require('./index');
+const chai = require('chai');
+chai.use(require('chai-things'));
+
+chai.should();
+const { expect } = chai;
+
+const cheapAsyncFn = () => new Promise(resolve => setImmediate(resolve));
+
+describe('Stopwatch', () => {
+  it('should work', () => {
+    const scope = src.make();
+    scope.registerCheckpoint('c1');
+    scope.saveDelta('c1', 'bucket');
+    scope.saveDelta('c1', 'bucket');
+    scope.saveDelta('c1', 'bucket');
+    scope.saveDelta('c1', 'bucket');
+    scope.saveDelta('c1', 'bucket');
+    scope.saveDelta('c1', 'bucket2');
+    scope.saveDelta('c1', 'bucket2');
+    scope.saveDelta('c1', 'bucket3');
+    scope.prettyPrint();
+    const buckets = scope.getPrettyBuckets();
+    expect(buckets.length).to.equal(3);
+    expect(buckets).all.to.have.property('bucketName');
+    expect(buckets).all.to.have.property('mean');
+    expect(buckets).all.to.have.property('std');
+    expect(buckets).all.to.have.property('max');
+    expect(buckets).all.to.have.property('min');
+    const bucketNames = buckets.map(({ bucketName }) => bucketName).sort();
+    expect(bucketNames).to.eql(['bucket', 'bucket2', 'bucket3']);
+  });
+  it('should wrap an async function', async () => {
+    const scope = src.make();
+    await scope.wrapFunction('fn', cheapAsyncFn)();
+    scope.prettyPrint();
+    const buckets = scope.getPrettyBuckets();
+    expect(buckets.length).to.equal(1);
+    expect(buckets[0]).to.have.property('bucketName', 'fn');
+    expect(buckets[0]).to.have.property('mean');
+    expect(buckets[0]).to.have.property('std');
+    expect(buckets[0]).to.have.property('max');
+    expect(buckets[0]).to.have.property('min');
+  });
+});
